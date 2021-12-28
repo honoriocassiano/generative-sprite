b11691c223b76ac3c78aa813f06657516474bf0f
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-12-28 05:46:54 -0300

    Add new type Sprite

--- src/main.rs
+++ src/main.rs
@@ -118,6 +118,50 @@ struct Arguments {
     pub sprite_lines: usize,
 }
 
+struct Sprite {
+    width: usize,
+    height: usize,
+    data: Vec<Color>,
+}
+
+impl Sprite {
+    pub fn new(width: usize, height: usize, data: Vec<Color>) -> Self {
+        Self {
+            width,
+            height,
+            data,
+        }
+    }
+
+    pub fn from_color(width: usize, height: usize, default_color: Color) -> Self {
+        Self {
+            width,
+            height,
+            data: vec![default_color].repeat(width * height),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn data(&self) -> &Vec<Color> {
+        &self.data
+    }
+
+    pub fn get_at(&self, line: usize, column: usize) -> Color {
+        self.data[matrix_index_to_vec(self.width)(line, column)]
+    }
+
+    pub fn set_at(&mut self, line: usize, column: usize, color: Color) {
+        self.data[matrix_index_to_vec(self.width)(line, column)] = color;
+    }
+}
+
 fn parse_arguments(args: Vec<String>) -> Arguments {
     let sprite_width = args[1].parse::<usize>().unwrap();
     let sprite_height = args[2].parse::<usize>().unwrap();
