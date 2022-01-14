0329f0e7d51b4c0565025d0512c97b3f3033a0e0
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2022-01-14 19:30:30 -0300

    Move argument parser to a new file

---
+++ src/argparser.rs
@@ -0,0 +1,94 @@
+use clap::{App, Arg};
+
+pub struct Arguments {
+    pub sprite_width: usize,
+    pub sprite_height: usize,
+    pub sprite_columns: usize,
+    pub sprite_lines: usize,
+
+    pub margin: usize,
+
+    pub seed: Option<[u8; 32]>,
+}
+
+pub fn parse_arguments() -> Arguments {
+    let matches = App::new("Generative")
+        .version(clap::crate_version!())
+        .about("Generate random sprites")
+        .arg(
+            Arg::with_name("sprite-width")
+                .help("Width (in pixels) by sprite")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("sprite-height")
+                .help("Height (in pixels) by sprite")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("sprite-columns")
+                .help("Number of columns of sprites matrix")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("sprite-lines")
+                .help("Number of lines of sprites matrix")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("margin")
+                .help("Size of margin between sprites")
+                .default_value("2")
+                .short("m")
+                .long("margin")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("seed")
+                .help("Seed to use")
+                .short("s")
+                .long("seed")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let sprite_width = matches
+        .value_of("sprite-width")
+        .unwrap()
+        .parse::<usize>()
+        .expect("Invalid sprite-width");
+    let sprite_height = matches
+        .value_of("sprite-height")
+        .unwrap()
+        .parse::<usize>()
+        .expect("Invalid sprite-height");
+    let sprite_columns = matches
+        .value_of("sprite-columns")
+        .unwrap()
+        .parse::<usize>()
+        .expect("Invalid sprite-columns");
+    let sprite_lines = matches
+        .value_of("sprite-lines")
+        .unwrap()
+        .parse::<usize>()
+        .expect("Invalid sprite-lines");
+    let margin = match matches.value_of("margin") {
+        None => 2,
+        Some(m) => m.parse::<usize>().expect("Invalid margin"),
+    };
+
+    let seed = matches.value_of("seed").map(|h| crate::parse_seed(h));
+
+    Arguments {
+        sprite_width,
+        sprite_height,
+        sprite_lines,
+        sprite_columns,
+        margin,
+        seed,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ use image::io::Reader;
 use image::{DynamicImage, ImageBuffer, Rgba};
 use rand::distributions::{Distribution, WeightedIndex};
 use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng, SeedableRng};
+use rand::{Rng, SeedableRng, thread_rng};
 use regex::Regex;
 
 use clap::{App, Arg};
 use rand::rngs::StdRng;
 use sprite::{Color, Sprite};
 use std::convert::TryInto;
+use crate::argparser::Arguments;
 
 mod sprite;
+mod argparser;
 
 #[derive(Copy, Clone)]
 struct Size(u32, u32);
@@ -105,99 +107,6 @@ fn generate_image(
         image_height as u32 * scale,
         FilterType::Nearest,
     )
-}
-
-struct Arguments {
-    pub sprite_width: usize,
-    pub sprite_height: usize,
-    pub sprite_columns: usize,
-    pub sprite_lines: usize,
-
-    pub margin: usize,
-
-    pub seed: Option<[u8; 32]>,
-}
-
-fn parse_arguments() -> Arguments {
-    let matches = App::new("Generative")
-        .version(clap::crate_version!())
-        .about("Generate random sprites")
-        .arg(
-            Arg::with_name("sprite-width")
-                .help("Width (in pixels) by sprite")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("sprite-height")
-                .help("Height (in pixels) by sprite")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("sprite-columns")
-                .help("Number of columns of sprites matrix")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("sprite-lines")
-                .help("Number of lines of sprites matrix")
-                .required(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("margin")
-                .help("Size of margin between sprites")
-                .default_value("2")
-                .short("m")
-                .long("margin")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("seed")
-                .help("Seed to use")
-                .short("s")
-                .long("seed")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let sprite_width = matches
-        .value_of("sprite-width")
-        .unwrap()
-        .parse::<usize>()
-        .expect("Invalid sprite-width");
-    let sprite_height = matches
-        .value_of("sprite-height")
-        .unwrap()
-        .parse::<usize>()
-        .expect("Invalid sprite-height");
-    let sprite_columns = matches
-        .value_of("sprite-columns")
-        .unwrap()
-        .parse::<usize>()
-        .expect("Invalid sprite-columns");
-    let sprite_lines = matches
-        .value_of("sprite-lines")
-        .unwrap()
-        .parse::<usize>()
-        .expect("Invalid sprite-lines");
-    let margin = match matches.value_of("margin") {
-        None => 2,
-        Some(m) => m.parse::<usize>().expect("Invalid margin"),
-    };
-
-    let seed = matches.value_of("seed").map(|h| parse_seed(h));
-
-    Arguments {
-        sprite_width,
-        sprite_height,
-        sprite_lines,
-        sprite_columns,
-        margin,
-        seed,
-    }
 }
 
 fn parse_seed(hash: &str) -> [u8; 32] {
@@ -368,7 +277,7 @@ fn remove_lonely_pixels(
 }
 
 fn main() {
-    let args = parse_arguments();
+    let args = argparser::parse_arguments();
 
     let background = Color::default();
 
